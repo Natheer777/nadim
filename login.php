cfd9da1886644fcfb19add2656be036b2d0783c0
Natheer_sharawi <111202840+Natheer777@users.noreply.github.com>
2024-11-10 14:53:39 +0300

    ff

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 
 
 <?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
 $conn = new mysqli("localhost", "root", "", "online_store");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
